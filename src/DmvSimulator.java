dbe3bc036b1c2232ba37475a7e49ca28f5b960fc
hle0 <91701075+hle0@users.noreply.github.com>
2022-03-02 13:35:00 -0500

    Make wait time more realistic.

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -5,11 +5,11 @@ public class DmvSimulator {
         System.out.println("Welcome to the DMV!");
 
         Random random = new Random();
-        int number = random.nextInt(1, 100);
+        int number = random.nextInt(1, 200);
         System.out.println("Your number is " + number + ". Please wait until it is called.");
 
-        for (int i = 0; i < 99; i++) {
-            int called = ((i + number) % 99) + 1;
+        for (int i = 0; i < 199; i++) {
+            int called = ((i + number) % 199) + 1;
             System.out.println("Number " + called + "? ... Number " + called + "?");
         }
 
