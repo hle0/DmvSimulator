713825029177aa988cda5e467010f269cd3be668
hle0 <91701075+hle0@users.noreply.github.com>
2022-03-02 13:30:23 -0500

    Setup classic DMV experience

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -1,5 +1,18 @@
+import java.util.Random;
+
 public class DmvSimulator {
     public static void main(String[] args) {
+        System.out.println("Welcome to the DMV!");
 
+        Random random = new Random();
+        int number = random.nextInt(1, 100);
+        System.out.println("Your number is " + number + ". Please wait until it is called.");
+
+        for (int i = 0; i < 99; i++) {
+            int called = ((i + number) % 99) + 1;
+            System.out.println("Number " + called + "? ... Number " + called + "?");
+        }
+
+        System.out.println("Sorry, you don't have the necessary paperwork.");
     }
 }
